c5abfd9477132b468a5c16cf270348e127e65586
Divyansh <divyanshmoonat@outlook.com>
2024-05-20 20:22:34 +0530

    Dot env added

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const jobRoutes = require("./routes/job");
 
 const app = express();
 
+dotenv.config();
+
 // JSON Parsing middleware
 app.use(express.json());
-
+console.log("URL=>", process.env.DB_CONNECTION_URL);
 // MongoDB Connection
 mongoose
-  .connect("mongodb+srv://divyansh1:AyXWypDYdCjXpQSC@jobapp.ahokicb.mongodb.net/") // Connection String
+  .connect(process.env.DB_CONNECTION_URL) // Connection String
   .then(() => console.log("Connection with Database established successfully"))
   .catch((err) => console.log("ERROR CONNECTING WITH DATABASE", err));
 
